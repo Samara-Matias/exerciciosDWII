432655510e39eb4d944281dbb1b3a9d95ef367a5
Samara Matias <154100944+Samara-Matias@users.noreply.github.com>
2025-10-21 17:39:57 -0300

    Parte dois - Semana 23

--- semana23/projeto-bancov01/api/modelo/Funcionario.php
+++ semana23/projeto-bancov01/api/modelo/Funcionario.php
@@ -6,17 +6,24 @@ require_once __DIR__ . '/Data.php';
 
 class Funcionario {
     private string $nome;
+    private string $cpf;
     private string $departamento;
     private float $salario;
     private bool $ativo;
     private Data $dataNascimento;
+    private static int $contador = 0;
+    private int $identificador = 0;
 
-    public function __construct(string $nome, string $departamento, float $salario, Data $dataNascimento) {
+    public function __construct(string $nome = '', string $departamento, float $salario, Data $dataNascimento, string $cpf = '') {
+        Funcionario::$contador++;
+        $this->identificador = self::$contador;
         $this->nome = $nome;
         $this->departamento = $departamento;
         $this->salario = $salario;
         $this->ativo = true;
         $this->dataNascimento = $dataNascimento;
+        if( isset( $nome ) )
+            $this->nome = $nome;
     }
 
     public function getNome(): string {
@@ -31,6 +38,10 @@ class Funcionario {
     	return $this->departamento; 
     }
 
+    public function getIdentificador(){
+        return $this->identificador;
+    }
+
     public function setDepartamento(string $departamento): void { 
     	$this->departamento = $departamento; 
     }
@@ -42,6 +53,15 @@ class Funcionario {
     public function setSalario(float $salario): void {
     	$this->salario = $salario; 
  	}
+
+    public function setCpf( string $cpf ):void{
+        if( $this->validaCpf( $cpf ) )
+            $this->cpf = $cpf;
+    }
+
+    public function getCpf():string{
+        return $this->cpf;
+    }
 
     public function isAtivo(): bool { 
     	return $this->ativo; 
@@ -63,12 +83,19 @@ class Funcionario {
         $this->ativo = true;
     }
 
+    public function validaCpf( string $cpf ):bool{
+        if( strlen( $cpf ) == 11 )
+            return true;
+        return false;
+    }
+
     public function exibeDados(): void {
-        echo "<br/>Nome: {$this->nome}<br>";
-        echo "Departamento: {$this->departamento}<br>";
-        echo "Salário: R$ " . $this->salario . "<br>";
-        echo "Ativo: " . ($this->ativo ? 'Sim' : 'Não') . "<br>";
-        echo "Data de Nascimento: " . $this->dataNascimento->getDataBR() . "<br>";
+        echo "<br/>Nome: {$this->nome}<br/>";
+        echo "Identificador: " . $this->identificador . "<br/>";
+        echo "Departamento: {$this->departamento}<br/>";
+        echo "Salário: R$ " . $this->salario . "<br/>";
+        echo "Ativo: " . ($this->ativo ? 'Sim' : 'Não') . "<br/>";
+        echo "Data de Nascimento: " . $this->dataNascimento->getDataBR() . "<br/>";
         echo "<hr>";
     }
 }
